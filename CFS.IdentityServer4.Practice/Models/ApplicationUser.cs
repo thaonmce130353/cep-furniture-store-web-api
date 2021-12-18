7cf5a90c451cea44e577ab1547d62d9e0552e4e2
thaonmce130353 <nguyenminhthaobt1@gmail.com>
2021-12-18 17:01:20 +0700

    Add Identity server 4 template

---
+++ CFS.IdentityServer4.Practice/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CFS.IdentityServer4.Practice.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
